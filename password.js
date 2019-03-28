668be94266ac0d13f7fb1f84b0d2a45672f32191
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-28 20:53:21 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,6 +1,18 @@
-var num = process.argv[2] ;
+var num = praseInt(process.argv[2], 10) ;
 var length = process.argv[3] ;
-console.log(num) ;
-console.log(length) ;
+
+function randomPassword(span) {
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < span; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    console.log(pass);
+}
+
+for(var i = 0; i < num; i++ ){
+  randomPassword(length)
+}
 
 process.exit(0) ;
